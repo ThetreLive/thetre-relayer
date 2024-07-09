40acc2189f7f4ea3176f2efae1a60291ae478677
Parth Gupta <parth.eng1210@gmail.com>
2024-07-09 14:57:29 +0530

    feat: Add Express server for multiaddress endpoint

--- relay.js
+++ relay.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
+import express from 'express';
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
@@ -9,9 +10,11 @@ import { webSockets } from '@libp2p/websockets'
 import * as filters from '@libp2p/websockets/filters'
 import { createLibp2p } from 'libp2p'
 
+const app = express();
+
 const server = await createLibp2p({
   addresses: {
-    listen: ['/ip4/127.0.0.1/tcp/0/ws']
+    listen: ['/ip4/0.0.0.0/tcp/0/ws']
   },
   transports: [
     webSockets({
@@ -32,5 +35,17 @@ const server = await createLibp2p({
     minConnections: 0
   }
 })
+
+app.get('/multiaddress', (req, res) => {
+    res.json({
+      multiaddress: server.getMultiaddrs().map((ma) => ma.toString())
+    });
+  });
+
+  // Start Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Express server running on port ${port}`);
+});
 
 console.log('Relay listening on multiaddr(s): ', server.getMultiaddrs().map((ma) => ma.toString()))
